4294dc0b26e90de85fa3ee707a90a974958f356a
Antonio Saragga <47437960+ASaragga@users.noreply.github.com>
2022-03-30 03:06:57 +0100

    add ETL

--- src/modulo03.jl
+++ src/modulo03.jl
@@ -1,3 +1,21 @@
+# Modelos de Volatilidade
+""" 
+    EWMA(retornos, lambda)  
+
+Calcula médias móveis exponencialmente ponderadas
+""" 
+function EWMA(retornos, lambda)
+    m = length(retornos)
+    media = mean(retornos)                 
+    sigma2 = 0
+    for k in 1:m
+        sigma2 += (1-lambda)*lambda^(k-1) * (retornos[end-k+1] - media)^2
+    end
+    return sqrt(sigma2)                     
+end
+
+
+# Abordagem Empírica
 function ETL(retornos, alfa, V)
     nobs = length(retornos)
     corte = quantile(retornos, alfa)
@@ -13,17 +31,53 @@ function ETL(retornos, alfa, V)
 end
 
 
-function EWMA(retornos, lambda)
-    m = length(retornos)
-    media = mean(retornos)                 
-    sigma2 = 0
-    for k in 1:m
-        sigma2 += (1-lambda)*lambda^(k-1) * (retornos[end-k+1] - media)^2
-    end
-    return sqrt(sigma2)                     
-end
+# Método Analítico
+""" 
+    VaR0(alfa, mu, sigma, V)
+
+Calcular o VaR-zero
+
+## Argumentos
+* `alfa`: nível de significância
+* `mu`: retorno esperado da carteira
+* `sigma`: desvio-padrão dos retornos da carteira
+* `V`: valor da carteira
+"""
+VaR0(alfa, mu, sigma,V) = (-quantile(Normal(0,1), alfa) * sigma - mu) * V
 
 
+""" 
+    VaRm(alfa, sigma, V)
+
+Calcular o VaR-média
+
+## Argumentos
+* `alfa`: nível de significância
+* `sigma`: desvio-padrão dos retornos da carteira
+* `V`: valor da carteira
+"""
+VaRm(alfa, sigma, V) = -quantile(Normal(0,1), alfa) * sigma * V
+
+
+""" 
+    ETL(alfa, mu, sigma, V)
+
+Calcular a Perda Esperda na Cauda
+
+## Argumentos
+* `alfa`: nível de significância
+* `mu`: retorno esperado da carteira
+* `sigma`: desvio-padrão dos retornos da carteira
+* `V`: valor da carteira
+"""
+ETL(alfa, mu, sigma, V) = (mu + sigma * pdf(Normal(0,1), quantile(Normal(0,1), alfa)) / alfa) * V
+
+
+""" 
+    semaforo(retornos, alfa)
+
+Teste do semáforo de Basileia
+""" 
 function semaforo(retornos, alfa)
     nobs = length(retornos)
     excep = zeros(nobs)
